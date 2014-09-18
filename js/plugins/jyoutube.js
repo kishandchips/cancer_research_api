eec49e05ef3b9e2d57877dfc99665dc8ffd2d021
franszo <josh@kishandchips.com>
2014-09-18 14:30:13 +0100

    fixing plain http requests to https
    Signed-off-by: franszo <josh@kishandchips.com>

--- js/plugins/jyoutube.js
+++ js/plugins/jyoutube.js
@@ -11,13 +11,13 @@ $.extend({
     vid = ( results === null ) ? url : results[1];
  
     if(size == "small"){
-      return "http://img.youtube.com/vi/"+vid+"/sddefault.jpg";
+      return "https://img.youtube.com/vi/"+vid+"/sddefault.jpg";
     } 
     else if(size == "smaller") {
-      return "http://img.youtube.com/vi/"+vid+"/hqdefault.jpg";
+      return "https://img.youtube.com/vi/"+vid+"/hqdefault.jpg";
     }
     else {
-      return "http://img.youtube.com/vi/"+vid+"/maxresdefault.jpg";
+      return "https://img.youtube.com/vi/"+vid+"/maxresdefault.jpg";
     }
   }
 });
